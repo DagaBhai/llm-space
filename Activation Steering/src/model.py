a285943ceec3f492487ae72add5b4cb682cd75b2
DagaBhai <dagayash14@gmail.com>
2026-06-19 18:12:36 +0530

    Add files via upload

---
+++ Activation Steering/src/model.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+tokenizer.pad_token = tokenizer.eos_token
+model.eval()
+
+
+print(model)
